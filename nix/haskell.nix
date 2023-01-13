4d75722719b4032f15f42f477c80c6a26fc11ce4
Barry <chiroptical@gmail.com>
2023-01-13 14:39:33 -0500

    Clean up

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -1,23 +1,8 @@
-{pkgs, ...}: let
-  http-api-data = pkgs.callPackage ./http-api-data.nix {};
-  attoparsec-iso8601 = pkgs.callPackage ./attoparsec-iso8601.nix {};
-in
-  pkgs.haskell.packages.ghc924.extend (final: prev: {
-    # "http-api-data" =
-    #   final.callCabal2nix
-    #   "http-api-data"
-    #   http-api-data
-    #   {};
-    #
-    # "attoparsec-iso8601" =
-    #   final.callCabal2nix
-    #   "attoparsec-iso8601"
-    #   "${attoparsec-iso8601}/attoparsec-iso8601"
-    #   {};
-    #
-    # "aeson" =
-    #   final.callCabal2nix
-    #   "aeson"
-    #   attoparsec-iso8601
-    #   {};
-  })
+{pkgs, ...}:
+pkgs.haskell.packages.ghc924.extend (final: prev: {
+  # "aeson" =
+  #   final.callCabal2nix
+  #   "aeson"
+  #   attoparsec-iso8601
+  #   {};
+})
--- nix/http-api-data.nix
+++
@@ -1,6 +0,0 @@
-{fetchgit, ...}:
-fetchgit {
-  url = "https://github.com/fizruk/http-api-data";
-  rev = "5408b078d1ee960309a9fc6141b7e14bc903b3a0";
-  sha256 = "sha256-x/hYny23aHSNMe9GDUmZdzrBSDObdP6d8rUEM/2p1mY=";
-}
